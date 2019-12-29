bd58f549cc2d36082eabfd39a13be46ffd442769
Jebil <jebil.kuruvilla@fingnet.com>
2019-12-29 11:50:10 +0530

    separate eureka

--- eureka-server/src/main/java/net/jk/app/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/net/jk/app/eurekaserver/EurekaServerApplication.java
@@ -1,20 +1,13 @@
 package net.jk.app.eurekaserver;
 
-import net.jk.app.commons.boot.VoilaCommonsSpringBootConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAsync
-@EnableTransactionManagement
-@EnableSwagger2
-@Import({VoilaCommonsSpringBootConfiguration.class})
 @EnableEurekaServer
 @EnableConfigServer
 public class EurekaServerApplication {
